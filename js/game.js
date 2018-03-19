cdaf39f7767e94abd52e117da1c30d1d51bce489
Andrew Ingraham <aingraham@flatiron.com>
2018-03-18 20:24:56 -0400

    Spiced things up

--- js/game.js
+++ js/game.js
@@ -1,27 +1,33 @@
+// NOTE - You must have a level with the name "start".  This is used as the first level in the game.
+
 var game = {
+    music: "98_Lost_Mine.mp3",
+    background_image: "intro-bg.jpg",
     levels: {
 
         start: {
             message: "You come across a dark cave",
             choices: [
                 {
-                text: "Enter the cave",
-                nextLevel: "cave",
+                    text: "Enter the cave",
+                    nextLevel: "cave",
                 },
 
                 {
-                text: "Keep on moving",
-                nextLevel: "field",
+                    text: "Keep on moving",
+                    nextLevel: "field",
                 },
             ]
         },
 
         cave: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
             message: "You're in a cave.  What'd you expect?",
             choices: [
                 {
-                text: "Start over",
-                nextLevel: "start",
+                    text: "Start over",
+                    nextLevel: "start",
                 },
             ]
         },
@@ -30,8 +36,8 @@ var game = {
             message: "Some adventurer you are...",
             choices: [
                 {
-                text: "Start over",
-                nextLevel: "start",
+                    text: "Start over",
+                    nextLevel: "start",
                 },
             ]
         },
--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
-
 $(document).ready(function() {
+  // Set up the first level
   var firstLevel = game.levels.start;
   renderLevel(firstLevel);
 
+  // Set background music if specified
+  setMusic(firstLevel);
+  setImage(firstLevel);
+  /*
+  if (game.music) {
+    $("#music").attr("src", "./music/" + game.music);
+  }
+  */
+
+  if (game.background_image) {
+    $("#background-image").css("background-image", "url(./img/" + game.background_image + ")");
+  }
+
+  // When user clicks a choice, go to the next level
   $("body").on("click", "#choices button", function() {
     var nextLevel = $(this).attr("data-next-level");
-    renderLevel(game.levels[nextLevel]);
+    $("#wrapper").animateCss("fadeOut", function() {
+      renderLevel(game.levels[nextLevel]);
+      $("#wrapper").removeClass("fadeOut");
+      $("#wrapper").addClass("fadeIn");
+    });
   });
 });
+
+var currentMusic = "";
 
 function renderLevel(level) {
   $("#prompt").text(level.message);
@@ -20,5 +40,64 @@ function renderLevel(level) {
       $("#choices").append("<button class='btn btn-outline-secondary' data-next-level='"  + choice.nextLevel + "'>" + choice.text + "</button>");
     }
   }
+
+  setMusic(level);
+  setImage(level);
 }
 
+function setMusic(level) {
+  var music = "";
+  if (level.music) {
+    music = level.music;
+  } else if (game.music) {
+    music = game.music;
+  }
+
+  if (currentMusic !== music) {
+    currentMusic = music;
+    $("#music").attr("src", "./music/" + currentMusic);
+  }
+}
+
+function setImage(level) {
+  var image = "";
+  if (level.background_image) {
+    image = level.background_image;
+  } else if (game.background_image) {
+    image = game.background_image;
+  }
+
+  $("#background-image").css("background-image", "url(./img/" + image + ")");
+  //$("#music").attr("src", "./music/" + currentMusic);
+  /*if (currentMusic !== music) {
+    currentMusic = music;
+    $("#music").attr("src", "./music/" + currentMusic);
+  }*/
+}
+
+$.fn.extend({
+  animateCss: function(animationName, callback) {
+    var animationEnd = (function(el) {
+      var animations = {
+        animation: 'animationend',
+        OAnimation: 'oAnimationEnd',
+        MozAnimation: 'mozAnimationEnd',
+        WebkitAnimation: 'webkitAnimationEnd',
+      };
+
+      for (var t in animations) {
+        if (el.style[t] !== undefined) {
+          return animations[t];
+        }
+      }
+    })(document.createElement('div'));
+
+    this.addClass('animated ' + animationName).one(animationEnd, function() {
+      //$(this).removeClass('animated ' + animationName);
+
+      if (typeof callback === 'function') callback();
+    });
+
+    return this;
+  },
+});
