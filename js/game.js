07cc15c5b81e2ccd10996f2d65c9a69b02e7dcf6
Andrew Ingraham <aingraham@flatiron.com>
2018-03-24 13:10:11 -0400

    Improved story

--- js/game.js
+++ js/game.js
@@ -20,18 +20,6 @@ var game = {
             ]
         },
 
-        cave: {
-            background_image: "fire.gif",
-            music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
-            choices: [
-                {
-                    text: "Start over",
-                    nextLevel: "start",
-                },
-            ]
-        },
-
         field: {
             message: "Some adventurer you are...",
             choices: [
@@ -42,5 +30,92 @@ var game = {
             ]
         },
 
+        cave: {
+            message: "Inside the cave, you can go left or right",
+            background_image: "fire.gif",
+            choices: [
+                {
+                    text: "Go left",
+                    nextLevel: "sword",
+                },
+                {
+                    text: "Go right",
+                    nextLevel: "dragonNoSword",
+                },
+            ]
+        },
+
+        sword: {
+            message: "You discover a sword and take it!",
+            background_image: "sword.jpg",
+            choices: [
+                {
+                    text: "Explore the rest of the cave",
+                    nextLevel: "dragonWithSword",
+                },
+            ]
+        },
+
+        dragonNoSword: {
+            message: "You encounter a dragon!",
+            background_image: "dragon2.jpg",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            choices: [
+                {
+                    text: "Attack the dragon!",
+                    nextLevel: "killedByDragon",
+                },
+                {
+                    text: "Run!!",
+                    nextLevel: "cowardlySurvival",
+                },
+            ]
+        },
+
+        dragonWithSword: {
+            message: "You encounter a dragon!",
+            background_image: "dragon2.jpg",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            choices: [
+                {
+                    text: "Attack the dragon!",
+                    nextLevel: "slayTheDragon",
+                },
+                {
+                    text: "Run!!",
+                    nextLevel: "cowardlySurvival",
+                },
+            ]
+        },
+
+        killedByDragon: {
+            message: "You die. You attacked a dragon without a weapon.  What'd you think would happen?",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+
+        cowardlySurvival: {
+            message: "You survive to live another cowardly day",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+
+        slayTheDragon: {
+            message: "Using your sword, you slay the dragon! That person you like is totally going to be into you now!",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
     }
 };
