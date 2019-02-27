16e831e50ecd4d0035e8e919dad62652d0cccd7f
Karina Montiel <karinamontiel375@gmail.com>
2019-02-27 05:17:42 +0000

    finished game

--- js/game.js
+++ js/game.js
@@ -55,22 +55,95 @@ var game = {
         },
         
             excited: {
-            background_image: "parkingLot.png",
+            background_image: "InsideVenue.png",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "Wow the there are so many seats! ",
+            message: "Wow the there are so many seats! Time to go on stage! ",
             choices: [
                 {
                     text: "Kill this Choreography",
-                    nextLevel: "Kill",
+                    nextLevel: "kill",
                 },
                 
                 {
-                    text: "",
-                    nextLevel: "",
+                    text: "Have a quick freak out and then chill",
+                    nextLevel: "kill",
                 },
             ]
         },
-
+             kill: {
+            background_image: "StageLights.jpg",
+            music: "Cheer.wav",
+            message: "Yes! You did amazing and even got a standing ovation!How will you accept first place?",
+            choices: [
+                {
+                    text: "With Grace and Gratitude ",
+                    nextLevel: "grace",
+                },
+                
+                {
+                    text: "Scream I Won! and then smuggly smile at the other teams.",
+                    nextLevel: "win",
+                },
+            ]
+        },
+     change: {
+            background_image: "ChangeRoom.png",
+            music: "talking.mp3",
+            message: "AS you enter many teams look intimidating. How will you respond?",
+            choices: [
+                {
+                    text: "Shake it off beacuse you know it is just a scare tactic",
+                    nextLevel: "kill",
+                },
+                
+                {
+                    text: "Mug them back! Who says you can't stoop low everyonce in a while.",
+                    nextLevel: "kill",
+                },
+            ]
+        },
+         grace: {
+            background_image: "win.jpg",
+            music: "Cheer.wav",
+            message: "Beautiful performance! Nice way to take the high road! See what happens when you are the best!",
+            choices: [
+                {
+                    text: "Start Over",
+                    nextLevel: "start",
+                },
+                
+                {
+                    text: "End Game",
+                    nextLevel: "end",
+                },
+                
+            ]
+        }, 
+                
+         win: {
+            background_image: "win.jpg",
+            music: "Cheer.wav",
+            message: "Okay! That performance did show them! See what happens when you are the best!",
+            choices: [
+                {
+                    text: "Start Over",
+                    nextLevel: "start",
+                },
+                
+                {
+                    text: "End Game",
+                    nextLevel: "end",
+                },
+                
+            ]
+        },
+        
+        end: {
+            background_image: "end.jpg",
+            music: "Cheer.wav",
+            message: "Great Game! Happy to see you play! Now you can say you have performed with a standing ovation!",
+            
+        },
     }
 };
 
